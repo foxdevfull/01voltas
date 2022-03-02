419cf455ffd11e41ab7e9ad439af2b31e8c9f67d
Douglas Santos <88425982+foxdevfull@users.noreply.github.com>
2022-03-02 20:33:36 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-
-import './App.css';
-
-function App() {
+import './App.css'
+const MostrarVoltas = (props) => {
+  return (
+    <p>
+      {props.voltas}
+      <br />
+      Voltas
+    </p>
+  )
+}
+const MostraTempo = (props) => {
+  return (
+    <p>
+      {props.tempo}
+      <br />
+      Tempo medio por volta
+    </p>
+  )
+}
+const Button = (props) => <button>{props.text}</button>
+function App(props) {
   return (
     <div className="App">
-     
+      <MostrarVoltas voltas="10" />
+      <Button text="+" />
+      <Button text="-" />
+      <MostraTempo tempo="01:02" />
+      <Button text="Iniciar" />
+      <Button text="Reiniciar" />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
