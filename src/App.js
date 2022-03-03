f30a66f5659b4574856421e42130d2bb162c5db9
Douglas Santos <88425982+foxdevfull@users.noreply.github.com>
2022-03-02 21:31:19 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,32 +1,53 @@
 import './App.css'
-const MostrarVoltas = (props) => {
-  return (
-    <p>
-      {props.voltas}
-      <br />
-      Voltas
-    </p>
-  )
-}
-const MostraTempo = (props) => {
-  return (
-    <p>
-      {props.tempo}
-      <br />
-      Tempo medio por volta
-    </p>
-  )
-}
-const Button = (props) => <button>{props.text}</button>
+import React from 'react'
+import { useState, useEffect } from 'react'
+import MostrarVoltas from './components/MostraVoltas'
+import MostraTempo from './components/MostraTempo'
+import Button from './components/Button'
+
 function App(props) {
+  const [numVoltas, setNumVoltas] = useState(0)
+  const [running, setRunning] = useState(false)
+  const [tempo, setTempo] = useState(0)
+
+  useEffect(() => {
+    let time = null
+    if (running) {
+      time = setInterval(() => {
+        setTempo((old) => old + 1)
+      }, 1000)
+    }
+    return () => {
+      if (time) {
+        clearInterval(time)
+      }
+    }
+  }, [running])
+
+  const toggleRunning = () => {
+    setRunning(!running)
+  }
+
+  const increment = () => {
+    setNumVoltas(numVoltas + 1)
+  }
+  const decrement = () => {
+    setNumVoltas(numVoltas - 1)
+  }
+  const reset = () => {
+    setRunning(!running)
+    setTempo(0)
+    setNumVoltas(0)
+  }
   return (
     <div className="App">
-      <MostrarVoltas voltas="10" />
-      <Button text="+" />
-      <Button text="-" />
-      <MostraTempo tempo="01:02" />
-      <Button text="Iniciar" />
-      <Button text="Reiniciar" />
+      <MostrarVoltas voltas={numVoltas} />
+      <Button text="+" onClick={increment} />
+      <Button text="-" onClick={decrement} />
+      {numVoltas > 0 && <MostraTempo tempo={Math.round(tempo / numVoltas)} />}
+
+      <Button text="Iniciar" onClick={toggleRunning} />
+      <Button text="Reiniciar" onClick={reset} />
     </div>
   )
 }
---
+++ src/components/MostraVoltas.js
@@ -0,0 +1,11 @@
+import React from 'react'
+const MostrarVoltas = (props) => {
+  return (
+    <p>
+      {props.voltas}
+      <br />
+      Voltas
+    </p>
+  )
+}
+export default MostrarVoltas
